9796e14cb2ed6f2c1493ecc69bd0059e2347a221
acej0k3r <ace.rav3n@gmail.com>
2025-03-02 22:04:57 -0500

    logo sends user to home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
    return (
       <div className="navbar flex flex-row justify-between items-center w-full min-h-[7vh]">
          <div className='px-4 animate-pulse duration-10000 ease-in opacity-100 hover:opacity-100'>
-            <h1 className='border-2 p-2 '>Community Board</h1>
+            <h1 className='border-2 p-2 '><Link to="/">Community Board</Link></h1>
          </div>
          <div className="links flex flex-row justify-end items-center w-[50%] pr-5 font-semibold">
             <div className='flex justify-end gap-6 w-full'>
